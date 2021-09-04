11e01560087b911d5b78e57efc0c143f72abb90b
Pradumn.Ojha <pradumn.ojha@skillassure.com>
2021-09-04 15:29:17 +0530

    Unit Test updated

---
+++ AppointmentUnitTest/Data/Repository/AppointmentRepoUnitTest.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace AppointmentUnitTest.Data.Repository
+{
+    [TestClass]
+    public class AppointmentRepoUnitTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
