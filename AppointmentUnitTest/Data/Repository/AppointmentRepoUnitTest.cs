235d5428a19e408d7689e5f3dfe5a5826ac88fa9
khushrm <reddykhushal@gmail.com>
2021-09-08 15:34:18 +0530

    Api repo

--- AppointmentUnitTest/Data/Repository/AppointmentRepoUnitTest.cs
+++ AppointmentUnitTest/Data/Repository/AppointmentRepoUnitTest.cs
@@ -1,14 +1,28 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using CMD.Appointment.Domain.Entities;
+using Data;
+using Data.Repositories;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Threading.Tasks;
 
 namespace AppointmentUnitTest.Data.Repository
 {
     [TestClass]
     public class AppointmentRepoUnitTest
     {
-        [TestMethod]
-        public void GetAllAppointments_ValidResult()
+        private ICollection<Appointment> GetAppointments()
         {
-            
+            return new List<Appointment>()
+            {
+                new Appointment(){Id=2, DoctorId=1, PatientId=1,Status="pending", AppointmentTime = new DateTime(2021, 08, 10)},
+                new Appointment(){Id=4, DoctorId=1, PatientId=1,Status="pending", AppointmentTime = new DateTime(2020, 05, 05)},
+                new Appointment(){Id=5, DoctorId=1, PatientId=1,Status="pending", AppointmentTime = new DateTime(2020, 05, 05)},
+                new Appointment(){Id=6, DoctorId=1, PatientId=1,Status="pending", AppointmentTime = new DateTime(2020, 05, 05)},
+                new Appointment(){Id=7, DoctorId=1, PatientId=1,Status="pending", AppointmentTime = new DateTime(2020, 05, 05)},
+            };
         }
     }
 }
